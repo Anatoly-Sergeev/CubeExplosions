d37564b9bae88ac1902ef662d8fc3890a70966d2
Anatoliy <sergeevbiz@ya.ru>
2026-01-18 18:17:44 +0300

    18/01 Alpha

--- Assets/Scripts/DividerCube.cs
+++ Assets/Scripts/DividerCube.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DividerCube
@@ -9,6 +10,8 @@ public class DividerCube
 
     [SerializeField] private FactoryCube _factory;
     [SerializeField] private InputReader _input;
+    [SerializeField] float _explosionForce;
+    [SerializeField] float _explosionRadius;
 
     private void OnEnable()
     {
@@ -22,7 +25,9 @@ public class DividerCube
 
     private void DividingCubes(Collider cube)
     {
-        _factory.CreateCubes(GetRandomCubesCount(), cube.transform.position, cube.transform.rotation, DecreaseCubeScale(cube.transform.localScale));
+        if (CanDividing(cube.transform.localScale.x))
+            ExplosionCubes(cube.transform.position, _factory.CreateCubes(GetRandomCubesCount(), cube.transform.position, cube.transform.rotation, DecreaseCubeScale(cube.transform.localScale)));
+
         Destroy(cube.gameObject);
     }
 
@@ -36,8 +41,14 @@ public class DividerCube
         return scale / ScaleReductionFactor;
     }
 
-    private void ExplosionCube()
+    private void ExplosionCubes(Vector3 explosionPosition, List<Rigidbody> cubes)
     {
+        foreach (Rigidbody cube in cubes)
+            cube.AddExplosionForce(_explosionForce, explosionPosition, _explosionRadius);
+    }
 
+    private bool CanDividing(float chance)
+    {
+        return Random.value <= chance;
     }
 }
--- Assets/Scripts/FactoryCube.cs
+++ Assets/Scripts/FactoryCube.cs
@@ -1,11 +1,14 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class FactoryCube : MonoBehaviour
 {
     [SerializeField] private GameObject _prefabCube;
 
-    public void CreateCubes(int count, Vector3 position, Quaternion rotation, Vector3 scale)
+    public List<Rigidbody> CreateCubes(int count, Vector3 position, Quaternion rotation, Vector3 scale)
     {
+        List<Rigidbody> cubes = new();
+
         for (int i = 0; i < count; i++)
         {
             GameObject cube = Instantiate(_prefabCube, position, rotation);
@@ -13,6 +16,11 @@ public class FactoryCube : MonoBehaviour
 
             if (cube.TryGetComponent<Renderer>(out Renderer render))
                 render.material.color = new(Random.value, Random.value, Random.value);
+
+            if (cube.TryGetComponent<Rigidbody>(out Rigidbody rigidbody))
+                cubes.Add(rigidbody);
         }
+
+        return cubes;
     }
 }
